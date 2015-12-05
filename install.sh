3bb53753273ec6c6488ef1c10041fbd411ced641
Tom Hale <tom@hale.ee>
2015-12-05 14:05:27 +0545

    Update submodules in install.sh

--- install.sh
+++ install.sh
@@ -7,10 +7,10 @@ die() {
 
 cd -P -- "$(dirname "$0")" || exit 1  # cd includes it's own fail message
 
-git submodule init
-git submodule update
+git pull && 
+git submodule init && 
+git submodule update && 
 git submodule foreach git pull origin master
-
 
 echo "\n** You'll need to symlink all the .dotfiles manually"
 
