c626229d42fab220d3da46b956bdd9b2a5e74cb5
Tom Hale <tom@hale.ee>
2015-12-05 12:28:02 +0545

    Add neobundle.vim as a submodule and install.sh to update it

--- install.sh
+++ install.sh
@@ -1,3 +1,16 @@
 #!/bin/sh
 
-# TODO!!!
+die() {
+    echo "$@: $?"
+    exit 1
+}
+
+cd -P -- "$(dirname "$0")" || exit 1  # cd includes it's own fail message
+
+git submodule init
+git submodule update
+git submodule foreach git pull origin master
+
+
+echo "\n** You'll need to symlink all the .dotfiles manually"
+
