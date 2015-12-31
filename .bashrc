d1e5e10fa1f8dae7d07405a9cb4ab2688683a89a
Tom Hale <tom@hale.ee>
2015-12-31 10:21:59 +0530

    "Various" - forgot to commit for a while

--- .bashrc
+++ .bashrc
@@ -1,12 +1,13 @@
+# rbenv setup
 eval "$(rbenv init -)"
-export PATH="$HOME/.rbenv/bin:$PATH"
-export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"
+export PATH="$HOME/.rbenv/bin:$HOME/.rbenv/plugins/ruby-build/bin:$PATH"
 
+PATH="$PATH:$HOME/bin"
 
 # Source .dotfiles listed at end of loop (one per line)
 while read dotfile ; do
     if [ -f "$dotfile" ]; then
-        echo "$dotfile"
+        # echo "$dotfile"
         .  "$dotfile"
     fi
 done <<DOTFILES
--- install.sh
+++ install.sh
@@ -8,9 +8,14 @@ die() {
 cd -P -- "$(dirname "$0")" || exit 1  # cd includes it's own fail message
 
 git pull && 
-git submodule init && 
-git submodule update && 
-git submodule foreach git pull origin master
+git submodule update --init --recursive
+# git submodule init && 
+# git submodule update && 
+# git submodule foreach git pull origin master
 
 echo "\n** You'll need to symlink all the .dotfiles manually"
-
+echo "(see \"map\" file)"
+echo
+echo "Install YCM vim plugin (just load vim) then:"
+echo "cd ~/.vim/bundle/YouCompleteMe"
+echo "./install.py --clang-completer"
